b1b7881dd4612222c8391ee651a7e7e17a7e91b6
EsotericPyramid <160424984+EsotericPyramid@users.noreply.github.com>
2025-04-18 17:08:14 -0400

    finished adding DynamicMatrix support for MatrixExpr

--- src/matrix/matrix_structs.rs
+++ src/matrix/matrix_structs.rs
@@ -229,9 +229,9 @@ impl<'a, T, const D1: usize, const D2: usize> Has2DReuseBuf for &'a mut [[T; D1]
 }
 
 
-#[inline] fn debox<T: Sized>(boxed: &mut Box<T>) -> &mut T {&mut *boxed}
+#[inline] fn debox<T: ?Sized>(boxed: &mut Box<T>) -> &mut T {&mut *boxed}
 
-unsafe impl<M: MatrixLike> Get2D for Box<M> {
+unsafe impl<M: MatrixLike + ?Sized> Get2D for Box<M> {
     type GetBool = M::GetBool;
     type AreInputsTransposed = M::AreInputsTransposed;
     type Inputs = M::Inputs;
@@ -243,7 +243,7 @@ unsafe impl<M: MatrixLike> Get2D for Box<M> {
     #[inline] fn process(&mut self, inputs: Self::Inputs) -> (Self::Item, Self::BoundItems) {(debox(self)).process(inputs)}
 }
 
-impl<M: MatrixLike> Has2DReuseBuf for Box<M> {
+impl<M: MatrixLike + ?Sized> Has2DReuseBuf for Box<M> {
     type FstHandleBool = M::FstHandleBool;
     type SndHandleBool = M::SndHandleBool;
     type BoundHandlesBool = M::BoundHandlesBool;
@@ -545,6 +545,52 @@ where
 }
 
 
+pub struct DynamicMatrixLike<M: MatrixLike>{pub(crate) mat: M, pub(crate) inputs: Option<M::Inputs>}
+
+unsafe impl<M: MatrixLike> Get2D for DynamicMatrixLike<M> {
+    type GetBool = M::GetBool;
+    type AreInputsTransposed = N;
+    type Inputs = ();
+    type Item = M::Item;
+    type BoundItems = M::BoundItems;
+
+    #[inline] unsafe fn get_inputs(&mut self, col_index: usize, row_index: usize) -> Self::Inputs { self.inputs = Some(unsafe {self.mat.get_inputs(col_index, row_index)}) }
+    #[inline] unsafe fn drop_inputs(&mut self, col_index: usize, row_index: usize) { unsafe {self.mat.drop_inputs(col_index, row_index)}}
+    #[inline] fn process(&mut self, _: Self::Inputs) -> (Self::Item, Self::BoundItems) {self.mat.process(self.inputs.take().unwrap())}
+}
+
+impl<M: MatrixLike> HasOutput for DynamicMatrixLike<M> {
+    type OutputBool = M::OutputBool;
+    type Output = M::Output;
+
+    #[inline] unsafe fn output(&mut self) -> Self::Output { unsafe {self.mat.output()}}
+    #[inline] unsafe fn drop_output(&mut self) { unsafe {self.mat.drop_output()}}
+}
+
+impl<M: MatrixLike> Has2DReuseBuf for DynamicMatrixLike<M> {
+    type FstHandleBool = M::FstHandleBool;
+    type SndHandleBool = M::SndHandleBool;
+    type BoundHandlesBool = M::BoundHandlesBool;
+    type FstOwnedBufferBool = M::FstOwnedBufferBool;
+    type SndOwnedBufferBool = M::SndOwnedBufferBool;
+    type IsFstBufferTransposed = M::IsFstBufferTransposed;
+    type IsSndBufferTransposed = M::IsSndBufferTransposed;
+    type AreBoundBuffersTransposed = M::AreBoundBuffersTransposed;
+    type FstOwnedBuffer = M::FstOwnedBuffer;
+    type SndOwnedBuffer = M::SndOwnedBuffer;
+    type FstType = M::FstType;
+    type SndType = M::SndType;
+    type BoundTypes = M::BoundTypes;
+
+    #[inline] unsafe fn assign_1st_buf(&mut self, col_index: usize, row_index: usize, val: Self::FstType) { unsafe {self.mat.assign_1st_buf(col_index, row_index, val)}}
+    #[inline] unsafe fn assign_2nd_buf(&mut self, col_index: usize, row_index: usize, val: Self::SndType) { unsafe {self.mat.assign_2nd_buf(col_index, row_index, val)}}
+    #[inline] unsafe fn assign_bound_bufs(&mut self, col_index: usize, row_index: usize, val: Self::BoundTypes) { unsafe {self.mat.assign_bound_bufs(col_index, row_index, val)}}
+    #[inline] unsafe fn get_1st_buffer(&mut self) -> Self::FstOwnedBuffer { unsafe {self.mat.get_1st_buffer()}}
+    #[inline] unsafe fn get_2nd_buffer(&mut self) -> Self::SndOwnedBuffer { unsafe {self.mat.get_2nd_buffer()}}
+    #[inline] unsafe fn drop_1st_buf_index(&mut self, col_index: usize, row_index: usize) { unsafe {self.mat.drop_1st_buf_index(col_index, row_index)}}
+    #[inline] unsafe fn drop_2nd_buf_index(&mut self, col_index: usize, row_index: usize) { unsafe {self.mat.drop_2nd_buf_index(col_index, row_index)}}
+    #[inline] unsafe fn drop_bound_bufs_index(&mut self, col_index: usize, row_index: usize) { unsafe {self.mat.drop_bound_bufs_index(col_index, row_index)}}
+}
 
 
 pub struct FullMatMul<M1: MatrixLike + IsRepeatable, M2: MatrixLike + IsRepeatable>{pub(crate) l_mat: M1, pub(crate) r_mat: M2, pub(crate) shared_size: usize}
