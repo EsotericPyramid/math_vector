7360f20c86a8247260d8b5ef678fc394e08171f0
EsotericPyramid <160424984+EsotericPyramid@users.noreply.github.com>
2025-04-21 11:39:50 -0400

    added additional ordering operations:     - Vec Reverse     - Mat Col & Row Reverse     - Mat Transpose     - Vec Offset Up     - Mat Offset Left     - Mat Offset Up
    and moved VecAttachUsedVec to correct file

--- src/vector/vector_structs/misc_vector_structs.rs
+++ src/vector/vector_structs/misc_vector_structs.rs
@@ -1,5 +1,6 @@
 use crate::util_traits::*;
 use crate::vector::vec_util_traits::*;
+use crate::trait_specialization_utils::*;
 
 
 pub struct VecBufSwap<T: VectorLike> {pub(crate) vec: T}
@@ -48,6 +49,92 @@ impl<T: VectorLike> HasReuseBuf for VecBufSwap<T> {
 }
 
 
+/// SAFETY: it is expected that the used_vec field is safe to output in addition to normal correct implementation
+pub struct VecAttachUsedVec<V: VectorLike, USEDV: VectorLike>{pub(crate) vec: V, pub(crate) used_vec: USEDV}
+
+unsafe impl<V: VectorLike, USEDV: VectorLike> Get for VecAttachUsedVec<V, USEDV> {
+    type GetBool = V::GetBool;
+    type Inputs = V::Inputs;
+    type Item = V::Item;
+    type BoundItems = V::BoundItems;
+
+    #[inline] unsafe fn get_inputs(&mut self, index: usize) -> Self::Inputs { unsafe {self.vec.get_inputs(index)}}
+    #[inline] unsafe fn drop_inputs(&mut self, index: usize) { unsafe {self.vec.drop_inputs(index)}}
+    #[inline] fn process(&mut self, inputs: Self::Inputs) -> (Self::Item, Self::BoundItems) {self.vec.process(inputs)}
+}
+
+unsafe impl<V: IsRepeatable + VectorLike, USEDV: VectorLike> IsRepeatable for VecAttachUsedVec<V, USEDV> {}
+
+impl<V: VectorLike, USEDV: VectorLike> HasOutput for VecAttachUsedVec<V, USEDV> where (V::OutputBool, USEDV::OutputBool): FilterPair {
+    type OutputBool = <(V::OutputBool, USEDV::OutputBool) as TyBoolPair>::Or;
+    type Output = <(V::OutputBool, USEDV::OutputBool) as FilterPair>::Filtered<V::Output, USEDV::Output>;
+
+    #[inline] 
+    unsafe fn output(&mut self) -> Self::Output { unsafe {
+        <(V::OutputBool, USEDV::OutputBool) as FilterPair>::filter(
+            self.vec.output(),
+            self.used_vec.output()
+        )
+    }}
+
+    #[inline]
+    unsafe fn drop_output(&mut self) { unsafe {
+        self.vec.drop_output();
+        self.used_vec.drop_output();
+    }}
+}
+
+impl<V: VectorLike, USEDV: VectorLike> HasReuseBuf for VecAttachUsedVec<V, USEDV> 
+where 
+    (V::FstOwnedBufferBool, USEDV::FstOwnedBufferBool): SelectPair,
+    (V::SndOwnedBufferBool, USEDV::SndOwnedBufferBool): SelectPair,
+    (V::FstHandleBool, USEDV::FstHandleBool): SelectPair,
+    (V::SndHandleBool, USEDV::SndHandleBool): SelectPair,
+    (V::BoundHandlesBool, USEDV::BoundHandlesBool): FilterPair
+{
+    type FstHandleBool = <(V::FstHandleBool, USEDV::FstHandleBool) as TyBoolPair>::Xor;
+    type SndHandleBool = <(V::SndHandleBool, USEDV::SndHandleBool) as TyBoolPair>::Xor;
+    type BoundHandlesBool = V::BoundHandlesBool;
+    type FstOwnedBufferBool = <(V::FstOwnedBufferBool, USEDV::FstOwnedBufferBool) as TyBoolPair>::Xor; 
+    type SndOwnedBufferBool = <(V::SndOwnedBufferBool, USEDV::SndOwnedBufferBool) as TyBoolPair>::Xor; 
+    type FstOwnedBuffer = <(V::FstOwnedBufferBool, USEDV::FstOwnedBufferBool) as SelectPair>::Selected<V::FstOwnedBuffer, USEDV::FstOwnedBuffer>;
+    type SndOwnedBuffer = <(V::SndOwnedBufferBool, USEDV::SndOwnedBufferBool) as SelectPair>::Selected<V::SndOwnedBuffer, USEDV::SndOwnedBuffer>;
+    type FstType = <(V::FstHandleBool, USEDV::FstHandleBool) as SelectPair>::Selected<V::FstType, USEDV::FstType>;
+    type SndType = <(V::SndHandleBool, USEDV::SndHandleBool) as SelectPair>::Selected<V::SndType, USEDV::SndType>;
+    type BoundTypes = V::BoundTypes;
+
+    #[inline] unsafe fn assign_1st_buf<'z>(&'z mut self, index: usize, val: Self::FstType) { unsafe {
+        let (l_val, r_val) = <(V::FstHandleBool, USEDV::FstHandleBool) as SelectPair>::deselect(val);
+        self.vec.assign_1st_buf(index, l_val);
+        self.used_vec.assign_1st_buf(index, r_val);
+    }}
+    #[inline] unsafe fn assign_2nd_buf<'z>(&'z mut self, index: usize, val: Self::SndType) { unsafe {
+        let (l_val, r_val) = <(V::SndHandleBool, USEDV::SndHandleBool) as SelectPair>::deselect(val);
+        self.vec.assign_2nd_buf(index, l_val);
+        self.used_vec.assign_2nd_buf(index, r_val);
+    }}
+    #[inline] unsafe fn assign_bound_bufs<'z>(&'z mut self, index: usize, val: Self::BoundTypes) { unsafe {
+        self.vec.assign_bound_bufs(index, val);
+    }}
+    #[inline] unsafe fn get_1st_buffer(&mut self) -> Self::FstOwnedBuffer { unsafe {
+        <(V::FstOwnedBufferBool, USEDV::FstOwnedBufferBool) as SelectPair>::select(self.vec.get_1st_buffer(), self.used_vec.get_1st_buffer())
+    }}
+    #[inline] unsafe fn get_2nd_buffer(&mut self) -> Self::SndOwnedBuffer { unsafe {
+        <(V::SndOwnedBufferBool, USEDV::SndOwnedBufferBool) as SelectPair>::select(self.vec.get_2nd_buffer(), self.used_vec.get_2nd_buffer())
+    }}
+    #[inline] unsafe fn drop_1st_buf_index(&mut self, index: usize) { unsafe {
+        self.vec.drop_1st_buf_index(index);
+        self.used_vec.drop_1st_buf_index(index);
+    }}
+    #[inline] unsafe fn drop_2nd_buf_index(&mut self, index: usize) { unsafe {
+        self.vec.drop_2nd_buf_index(index);
+        self.used_vec.drop_2nd_buf_index(index);
+    }}
+    #[inline] unsafe fn drop_bound_bufs_index(&mut self, index: usize) { unsafe {
+        self.vec.drop_bound_bufs_index(index);
+        self.used_vec.drop_bound_bufs_index(index);
+    }}
+}
 
 pub struct DynamicVectorLike<V: VectorLike>{pub(crate) vec: V, pub(crate) inputs: Option<V::Inputs>}
 
--- src/vector/vector_structs/ordering_vector_structs.rs
+++ src/vector/vector_structs/ordering_vector_structs.rs
@@ -1,4 +1,4 @@
-use crate::trait_specialization_utils::*;
+
 use crate::util_traits::*;
 use crate::vector::vec_util_traits::*;
 
@@ -22,7 +22,7 @@ impl<T: VectorLike> VecOffset<T> {
 }
 
 unsafe impl<T: VectorLike> Get for VecOffset<T> {
-    type GetBool = T::GetBool; // NOTE: N because offset_index adds a small amount of extra computation on get
+    type GetBool = T::GetBool;
     type Inputs = T::Inputs;
     type Item = T::Item;
     type BoundItems = T::BoundItems;
@@ -62,89 +62,53 @@ impl<T: VectorLike> HasReuseBuf for VecOffset<T> {
     #[inline] unsafe fn drop_bound_bufs_index(&mut self, index: usize) { unsafe {self.vec.drop_bound_bufs_index(self.offset_index(index))}}
 }
 
-/// SAFETY: it is expected that the used_vec field is safe to output in addition to normal correct implementation
-pub struct VecAttachUsedVec<V: VectorLike, USEDV: VectorLike>{pub(crate) vec: V, pub(crate) used_vec: USEDV}
 
-unsafe impl<V: VectorLike, USEDV: VectorLike> Get for VecAttachUsedVec<V, USEDV> {
-    type GetBool = V::GetBool;
-    type Inputs = V::Inputs;
-    type Item = V::Item;
-    type BoundItems = V::BoundItems;
+pub struct VecReverse<V: VectorLike>{pub(crate) vec: V, pub(crate) max_index: usize}
 
-    #[inline] unsafe fn get_inputs(&mut self, index: usize) -> Self::Inputs { unsafe {self.vec.get_inputs(index)}}
-    #[inline] unsafe fn drop_inputs(&mut self, index: usize) { unsafe {self.vec.drop_inputs(index)}}
+impl<T: VectorLike> VecReverse<T> {
+    #[inline]
+    fn reverse_index(&self, index: usize) -> usize {
+        self.max_index - index
+    }
+}
+
+unsafe impl<T: VectorLike> Get for VecReverse<T> {
+    type GetBool = T::GetBool;
+    type Inputs = T::Inputs;
+    type Item = T::Item;
+    type BoundItems = T::BoundItems;
+
+    #[inline] unsafe fn get_inputs(&mut self, index: usize) -> Self::Inputs { unsafe {self.vec.get_inputs(self.reverse_index(index))}}
+    #[inline] unsafe fn drop_inputs(&mut self, index: usize) { unsafe {self.vec.drop_inputs(self.reverse_index(index))}}
     #[inline] fn process(&mut self, inputs: Self::Inputs) -> (Self::Item, Self::BoundItems) {self.vec.process(inputs)}
 }
 
-unsafe impl<V: IsRepeatable + VectorLike, USEDV: VectorLike> IsRepeatable for VecAttachUsedVec<V, USEDV> {}
+impl<T: VectorLike> HasOutput for VecReverse<T> {
+    type OutputBool = T::OutputBool;
+    type Output = T::Output;
 
-impl<V: VectorLike, USEDV: VectorLike> HasOutput for VecAttachUsedVec<V, USEDV> where (V::OutputBool, USEDV::OutputBool): FilterPair {
-    type OutputBool = <(V::OutputBool, USEDV::OutputBool) as TyBoolPair>::Or;
-    type Output = <(V::OutputBool, USEDV::OutputBool) as FilterPair>::Filtered<V::Output, USEDV::Output>;
-
-    #[inline] 
-    unsafe fn output(&mut self) -> Self::Output { unsafe {
-        <(V::OutputBool, USEDV::OutputBool) as FilterPair>::filter(
-            self.vec.output(),
-            self.used_vec.output()
-        )
-    }}
-
-    #[inline]
-    unsafe fn drop_output(&mut self) { unsafe {
-        self.vec.drop_output();
-        self.used_vec.drop_output();
-    }}
+    #[inline] unsafe fn output(&mut self) -> Self::Output { unsafe {self.vec.output()}}
+    #[inline] unsafe fn drop_output(&mut self) { unsafe {self.vec.drop_output()}}
 }
 
-impl<V: VectorLike, USEDV: VectorLike> HasReuseBuf for VecAttachUsedVec<V, USEDV> 
-where 
-    (V::FstOwnedBufferBool, USEDV::FstOwnedBufferBool): SelectPair,
-    (V::SndOwnedBufferBool, USEDV::SndOwnedBufferBool): SelectPair,
-    (V::FstHandleBool, USEDV::FstHandleBool): SelectPair,
-    (V::SndHandleBool, USEDV::SndHandleBool): SelectPair,
-    (V::BoundHandlesBool, USEDV::BoundHandlesBool): FilterPair
-{
-    type FstHandleBool = <(V::FstHandleBool, USEDV::FstHandleBool) as TyBoolPair>::Xor;
-    type SndHandleBool = <(V::SndHandleBool, USEDV::SndHandleBool) as TyBoolPair>::Xor;
-    type BoundHandlesBool = V::BoundHandlesBool;
-    type FstOwnedBufferBool = <(V::FstOwnedBufferBool, USEDV::FstOwnedBufferBool) as TyBoolPair>::Xor; 
-    type SndOwnedBufferBool = <(V::SndOwnedBufferBool, USEDV::SndOwnedBufferBool) as TyBoolPair>::Xor; 
-    type FstOwnedBuffer = <(V::FstOwnedBufferBool, USEDV::FstOwnedBufferBool) as SelectPair>::Selected<V::FstOwnedBuffer, USEDV::FstOwnedBuffer>;
-    type SndOwnedBuffer = <(V::SndOwnedBufferBool, USEDV::SndOwnedBufferBool) as SelectPair>::Selected<V::SndOwnedBuffer, USEDV::SndOwnedBuffer>;
-    type FstType = <(V::FstHandleBool, USEDV::FstHandleBool) as SelectPair>::Selected<V::FstType, USEDV::FstType>;
-    type SndType = <(V::SndHandleBool, USEDV::SndHandleBool) as SelectPair>::Selected<V::SndType, USEDV::SndType>;
-    type BoundTypes = V::BoundTypes;
+impl<T: VectorLike> HasReuseBuf for VecReverse<T> {
+    type FstHandleBool = T::FstHandleBool;
+    type SndHandleBool = T::SndHandleBool;
+    type BoundHandlesBool = T::BoundHandlesBool;
+    type FstOwnedBufferBool = T::FstOwnedBufferBool;
+    type SndOwnedBufferBool = T::SndOwnedBufferBool;
+    type FstOwnedBuffer = T::FstOwnedBuffer;
+    type SndOwnedBuffer = T::SndOwnedBuffer;
+    type FstType = T::FstType;
+    type SndType = T::SndType;
+    type BoundTypes = T::BoundTypes;
 
-    #[inline] unsafe fn assign_1st_buf<'z>(&'z mut self, index: usize, val: Self::FstType) { unsafe {
-        let (l_val, r_val) = <(V::FstHandleBool, USEDV::FstHandleBool) as SelectPair>::deselect(val);
-        self.vec.assign_1st_buf(index, l_val);
-        self.used_vec.assign_1st_buf(index, r_val);
-    }}
-    #[inline] unsafe fn assign_2nd_buf<'z>(&'z mut self, index: usize, val: Self::SndType) { unsafe {
-        let (l_val, r_val) = <(V::SndHandleBool, USEDV::SndHandleBool) as SelectPair>::deselect(val);
-        self.vec.assign_2nd_buf(index, l_val);
-        self.used_vec.assign_2nd_buf(index, r_val);
-    }}
-    #[inline] unsafe fn assign_bound_bufs<'z>(&'z mut self, index: usize, val: Self::BoundTypes) { unsafe {
-        self.vec.assign_bound_bufs(index, val);
-    }}
-    #[inline] unsafe fn get_1st_buffer(&mut self) -> Self::FstOwnedBuffer { unsafe {
-        <(V::FstOwnedBufferBool, USEDV::FstOwnedBufferBool) as SelectPair>::select(self.vec.get_1st_buffer(), self.used_vec.get_1st_buffer())
-    }}
-    #[inline] unsafe fn get_2nd_buffer(&mut self) -> Self::SndOwnedBuffer { unsafe {
-        <(V::SndOwnedBufferBool, USEDV::SndOwnedBufferBool) as SelectPair>::select(self.vec.get_2nd_buffer(), self.used_vec.get_2nd_buffer())
-    }}
-    #[inline] unsafe fn drop_1st_buf_index(&mut self, index: usize) { unsafe {
-        self.vec.drop_1st_buf_index(index);
-        self.used_vec.drop_1st_buf_index(index);
-    }}
-    #[inline] unsafe fn drop_2nd_buf_index(&mut self, index: usize) { unsafe {
-        self.vec.drop_2nd_buf_index(index);
-        self.used_vec.drop_2nd_buf_index(index);
-    }}
-    #[inline] unsafe fn drop_bound_bufs_index(&mut self, index: usize) { unsafe {
-        self.vec.drop_bound_bufs_index(index);
-        self.used_vec.drop_bound_bufs_index(index);
-    }}
+    #[inline] unsafe fn assign_1st_buf(&mut self, index: usize, val: Self::FstType) { unsafe {self.vec.assign_1st_buf(self.reverse_index(index), val)}}
+    #[inline] unsafe fn assign_2nd_buf(&mut self, index: usize, val: Self::SndType) { unsafe {self.vec.assign_2nd_buf(self.reverse_index(index), val)}}
+    #[inline] unsafe fn assign_bound_bufs(&mut self, index: usize, val: Self::BoundTypes) { unsafe {self.vec.assign_bound_bufs(self.reverse_index(index), val)}}
+    #[inline] unsafe fn get_1st_buffer(&mut self) -> Self::FstOwnedBuffer { unsafe {self.vec.get_1st_buffer()}}
+    #[inline] unsafe fn get_2nd_buffer(&mut self) -> Self::SndOwnedBuffer { unsafe {self.vec.get_2nd_buffer()}}
+    #[inline] unsafe fn drop_1st_buf_index(&mut self, index: usize) { unsafe {self.vec.drop_1st_buf_index(self.reverse_index(index))}}
+    #[inline] unsafe fn drop_2nd_buf_index(&mut self, index: usize) { unsafe {self.vec.drop_2nd_buf_index(self.reverse_index(index))}}
+    #[inline] unsafe fn drop_bound_bufs_index(&mut self, index: usize) { unsafe {self.vec.drop_bound_bufs_index(self.reverse_index(index))}}
 }
