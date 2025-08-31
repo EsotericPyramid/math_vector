af07bb08ea1434c654c52b93ade84c44a75998bc
EsotericPyramid <160424984+EsotericPyramid@users.noreply.github.com>
2025-08-30 20:21:20 -0400

    added VecRawBind and fixed VecEvalOps

--- src/vector/vector_structs/binding_vector_structs.rs
+++ src/vector/vector_structs/binding_vector_structs.rs
@@ -78,6 +78,80 @@ impl<V: VectorLike<FstHandleBool = Y>> HasReuseBuf for VecBind<V> where (V::Boun
     }}
 }
 
+/// struct binding the vector's item to the buffer in the first slot which is linked to output without cleanly filtering the output
+pub struct VecRawBind<V: VectorLike<FstHandleBool = Y>> {pub(crate) vec: V} 
+
+unsafe impl<V: VectorLike<FstHandleBool = Y>> Get for VecRawBind<V> where (V::BoundHandlesBool, Y): FilterPair {
+    type GetBool = N;
+    type Inputs = V::Inputs;
+    type Item = ();
+    type BoundItems = <(V::BoundHandlesBool, Y) as FilterPair>::Filtered<V::BoundItems, V::Item>;
+
+    #[inline]
+    unsafe fn get_inputs(&mut self, index: usize) -> Self::Inputs { unsafe {
+        self.vec.get_inputs(index)
+    }}
+
+    #[inline]
+    unsafe fn drop_inputs(&mut self, index: usize) { unsafe {
+        self.vec.drop_inputs(index)
+    }}
+
+    #[inline]
+    fn process(&mut self, index: usize, inputs: Self::Inputs) -> (Self::Item, Self::BoundItems) {
+        let (item, bound) = self.vec.process(index, inputs);
+        ((), <(V::BoundHandlesBool, Y) as FilterPair>::filter(bound, item))
+    }
+}
+
+impl<V: VectorLike<FstHandleBool = Y>> HasOutput for VecRawBind<V> where (V::OutputBool, V::FstOwnedBufferBool): TyBoolPair {
+    type OutputBool = <(V::OutputBool, V::FstOwnedBufferBool) as TyBoolPair>::Or;
+    type Output = (V::Output, V::FstOwnedBuffer);
+    
+    #[inline]
+    unsafe fn output(&mut self) -> Self::Output { unsafe {
+        (self.vec.output(), self.vec.get_1st_buffer())
+    }}
+
+    #[inline]
+    unsafe fn drop_output(&mut self) { unsafe {
+        self.vec.drop_output(); // buffer dropped through HasReuseBuf
+        self.vec.drop_1st_buffer();
+    }}
+}
+
+impl<V: VectorLike<FstHandleBool = Y>> HasReuseBuf for VecRawBind<V> where (V::BoundHandlesBool, Y): FilterPair {
+    type FstHandleBool = N;
+    type SndHandleBool = V::SndHandleBool;
+    type BoundHandlesBool = <(V::BoundHandlesBool, Y) as TyBoolPair>::Or;
+    type FstOwnedBufferBool = N;
+    type SndOwnedBufferBool = V::SndOwnedBufferBool;
+    type FstOwnedBuffer = ();
+    type SndOwnedBuffer = V::SndOwnedBuffer;
+    type FstType = ();
+    type SndType = V::SndType;
+    type BoundTypes = <(V::BoundHandlesBool, Y) as FilterPair>::Filtered<V::BoundTypes, V::FstType>;
+
+    #[inline] unsafe fn assign_1st_buf(&mut self, _: usize, _: Self::FstType) {}
+    #[inline] unsafe fn assign_2nd_buf(&mut self, index: usize, val: Self::SndType) { unsafe {self.vec.assign_2nd_buf(index, val)}}
+    #[inline] unsafe fn assign_bound_bufs(&mut self, index: usize, val: Self::BoundTypes) { unsafe {
+        let (bounded_vals, new_bound_val) = <(V::BoundHandlesBool, Y) as FilterPair>::defilter(val);
+        self.vec.assign_bound_bufs(index, bounded_vals);
+        self.vec.assign_1st_buf(index, new_bound_val);
+    }}
+    #[inline] unsafe fn get_1st_buffer(&mut self) -> Self::FstOwnedBuffer {}
+    #[inline] unsafe fn get_2nd_buffer(&mut self) -> Self::SndOwnedBuffer { unsafe {self.vec.get_2nd_buffer()}}
+    #[inline] unsafe fn drop_1st_buffer(&mut self) {}
+    #[inline] unsafe fn drop_2nd_buffer(&mut self) { unsafe {self.vec.drop_2nd_buffer()}}
+    #[inline] unsafe fn drop_1st_buf_index(&mut self, _: usize) {}
+    #[inline] unsafe fn drop_2nd_buf_index(&mut self, index: usize) { unsafe {self.vec.drop_2nd_buf_index(index)}}
+    #[inline] unsafe fn drop_bound_bufs_index(&mut self, index: usize) { unsafe {
+        self.vec.drop_1st_buf_index(index);
+        self.vec.drop_bound_bufs_index(index);
+    }}
+}
+
+
 /// struct maping the vector's item and binding part of the output to the 
 /// buffer in the first slot which is linked to output and the other part as the new item
 pub struct VecMapBind<V: VectorLike<FstHandleBool = Y>, F: FnMut(V::Item) -> (I, B), I, B> {pub(crate) vec: V, pub(crate) f: F}
