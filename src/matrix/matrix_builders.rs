de53320ddce4db4909cb3a3023d55cd2b23f8faf
EsotericPyramid <160424984+EsotericPyramid@users.noreply.github.com>
2026-05-09 14:58:22 -0400

    made RSMatrixExprBuilder

--- src/matrix/matrix_builders.rs
+++ src/matrix/matrix_builders.rs
@@ -3,9 +3,10 @@
 use crate::{
     matrix::{
         MatrixExpr,
+        RSMatrixExpr,
         mat_util_traits::{MatrixBuilder, MatrixBuilderCompose, MatrixBuilderUnion, MatrixLike},
     },
-    vector::{VectorExpr, vec_util_traits::VectorLike, vector_builders::VectorExprBuilder},
+    vector::{VectorExpr, RSVectorExpr, vec_util_traits::VectorLike, vector_builders::*},
 };
 
 /// a simple const sized MatrixBuilder
@@ -49,6 +50,87 @@ impl<const D1: usize, const D2: usize> MatrixBuilder for MatrixExprBuilder<D1, D
     }
 }
 
+impl<const D1: usize, const D2: usize> MatrixBuilderCompose<VectorExprBuilder<D2>>
+    for VectorExprBuilder<D1>
+{
+    type Composition = MatrixExprBuilder<D1, D2>;
+
+    fn compose(self, _: VectorExprBuilder<D2>) -> Self::Composition {
+        MatrixExprBuilder
+    }
+}
+
+#[derive(Clone)]
+pub struct RSMatrixExprBuilder{
+    pub(crate) num_rows: usize,
+    pub(crate) num_cols: usize,
+}
+
+impl MatrixBuilder for RSMatrixExprBuilder {
+    type MatrixWrapped<T: MatrixLike> = RSMatrixExpr<T>;
+    type TransposedMatrixWrapped<T: MatrixLike> = RSMatrixExpr<T>;
+    type ColWrapped<T: VectorLike> = RSVectorExpr<T>;
+    type RowWrapped<T: VectorLike> = RSVectorExpr<T>;
+
+    type ColBuilder = RSVectorExprBuilder;
+    type RowBuilder = RSVectorExprBuilder;
+
+    #[inline]
+    unsafe fn wrap_mat<T: MatrixLike>(&self, mat: T) -> Self::MatrixWrapped<T> {
+        RSMatrixExpr{
+            mat,
+            num_rows: self.num_rows,
+            num_cols: self.num_cols,
+        }
+    }
+    #[inline]
+    unsafe fn wrap_trans_mat<T: MatrixLike>(&self, mat: T) -> Self::TransposedMatrixWrapped<T> {
+        RSMatrixExpr{
+            mat,
+            num_rows: self.num_cols,
+            num_cols: self.num_rows
+        }
+    }
+    #[inline]
+    unsafe fn wrap_col_vec<T: VectorLike>(&self, vec: T) -> Self::ColWrapped<T> {
+        RSVectorExpr{
+            vec,
+            size: self.num_rows
+        }
+    }
+    #[inline]
+    unsafe fn wrap_row_vec<T: VectorLike>(&self, vec: T) -> Self::RowWrapped<T> {
+        RSVectorExpr{
+            vec,
+            size: self.num_cols
+        }
+    }
+
+    #[inline]
+    fn decompose(self) -> (Self::ColBuilder, Self::RowBuilder) {
+        (RSVectorExprBuilder{size: self.num_rows}, RSVectorExprBuilder{size: self.num_cols})
+    }
+
+    #[inline]
+    fn dimensions(&self) -> (usize, usize) {
+        (self.num_rows, self.num_cols)
+    }
+}
+
+impl MatrixBuilderCompose<RSVectorExprBuilder> for RSVectorExprBuilder {
+    type Composition = RSMatrixExprBuilder;
+
+    #[inline]
+    fn compose(self, other: RSVectorExprBuilder) -> Self::Composition {
+        RSMatrixExprBuilder{
+            num_rows: self.size,
+            num_cols: other.size,
+        }
+    }
+}
+
+
+
 impl<const D1: usize, const D2: usize> MatrixBuilderUnion<MatrixExprBuilder<D1, D2>>
     for MatrixExprBuilder<D1, D2>
 {
@@ -59,12 +141,33 @@ impl<const D1: usize, const D2: usize> MatrixBuilderUnion<MatrixExprBuilder<D1, 
     }
 }
 
-impl<const D1: usize, const D2: usize> MatrixBuilderCompose<VectorExprBuilder<D2>>
-    for VectorExprBuilder<D1>
+impl<const D1: usize, const D2: usize> MatrixBuilderUnion<MatrixExprBuilder<D1, D2>>
+    for RSMatrixExprBuilder
 {
-    type Composition = MatrixExprBuilder<D1, D2>;
+    type Union = MatrixExprBuilder<D1, D2>;
 
-    fn compose(self, _: VectorExprBuilder<D2>) -> Self::Composition {
+    fn union(self, other: MatrixExprBuilder<D1, D2>) -> Self::Union {
+        assert_eq!(self.dimensions(), other.dimensions(), "math_vector error: cannot combine 2 matrixes of different size");
         MatrixExprBuilder
+    }
+}
+
+impl<const D1: usize, const D2: usize> MatrixBuilderUnion<RSMatrixExprBuilder>
+    for MatrixExprBuilder<D1, D2>
+{
+    type Union = MatrixExprBuilder<D1, D2>;
+
+    fn union(self, other: RSMatrixExprBuilder) -> Self::Union {
+        assert_eq!(self.dimensions(), other.dimensions(), "math_vector error: cannot combine 2 matrixes of different size");
+        MatrixExprBuilder
+    }
+}
+
+impl MatrixBuilderUnion<RSMatrixExprBuilder> for RSMatrixExprBuilder {
+    type Union = Self;
+
+    fn union(self, other: RSMatrixExprBuilder) -> Self::Union {
+        assert_eq!(self.dimensions(), other.dimensions(), "math_vector error: cannot combine 2 matrixes of different size");
+        self
     }
 }
